bbb550286ccea46d1589b3854dc8954138984e3e
alanx <alanx99@gmail.com>
2021-02-21 02:55:42 -0500

    auth done

--- NUfriends/App.tsx
+++ NUfriends/App.tsx
@@ -8,9 +8,10 @@ import Navigation from "./navigation";
 
 import Amplify from "aws-amplify";
 import config from "./aws-exports";
+import { withAuthenticator } from "aws-amplify-react-native";
 Amplify.configure(config);
 
-export default function App() {
+function App() {
   const isLoadingComplete = useCachedResources();
   const colorScheme = useColorScheme();
 
@@ -25,3 +26,5 @@ export default function App() {
     );
   }
 }
+
+export default withAuthenticator(App);
